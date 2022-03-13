b57e183959a55ca60d872b9d6be0cd560a92e286
Gustavo <martins0712@hotmail.com>
2022-03-13 20:34:45 -0300

    finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -11,13 +11,8 @@ export function Header() {
             mx="auto"
             mt="4"
             px="6"
-            align="center"
-        >
-            <Text
-                fontSize="3xl"
-                fontWeight="bold"
-                letterSpacing="tight"
-                w="64">
+            align="center">
+            <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
                 up agency
                 <Text as="span" ml="1" color="pink.500">.</Text>
             </Text>
@@ -35,15 +30,33 @@ export function Header() {
                 bg="gray.800"
                 borderRadius="full"
             >
-                <Input
-                    color="gray.200"
-                    variant="unstyled"
-                    placeholder='Buscar na plataforma'
-                    _placeholder={{ color: "gray.400" }}
-                >
-
-                </Input>
+                <Input color="gray.200" variant="unstyled" placeholder='Buscar na plataforma' _placeholder={{ color: "gray.400" }} />
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex align="center" ml="auto">
+                <HStack
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Gustavo Martins</Text>
+                        <Text color="gray.300">martins0712@hotmail.com</Text>
+                    </Box>
+
+                    <Avatar size="md" name="Gustavo Martins" src="https://github.com/guga0712.png" />
+                </Flex>
+
             </Flex>
 
         </Flex >
