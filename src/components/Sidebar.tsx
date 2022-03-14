7061f3c365691cabff1bb093337ce8eb633c94e4
Gustavo <martins0712@hotmail.com>
2022-03-13 21:29:28 -0300

    created side bar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { RiBankLine, RiDashboardLine, RiUser3Line } from "react-icons/ri";
+
+export function Sidebar() {
+    return (
+        <Box as="aside" w="64" mr="8">
+            <Stack spacing="12" align="flex-start">
+                <Box>
+                    <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
+                    <Stack spacing="4" mt="8" align="stretch">
+                        <Link display="flex" align="center">
+                            <Icon as={RiDashboardLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Dashboard</Text>
+                        </Link>
+                        <Link display="flex" align="center">
+                            <Icon as={RiUser3Line} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Clientes</Text>
+                        </Link>
+                        <Link display="flex" align="center">
+                            <Icon as={RiBankLine} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">Financeiro</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
+    );
+}
