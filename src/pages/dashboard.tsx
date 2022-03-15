d1406986bcceb5c115e3d9539ccb54a672fb513f
guga0712 <martins0712@hotmail.com>
2022-03-15 17:00:30 -0300

    added user page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,7 +7,6 @@ export default function Dashboard() {
     return (
         <Flex direction="column" h="100vh">
             <Header />
-
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
                 <Flex flexDirection="column" gap="4" bg="gray.800" p="30px" borderRadius="10px" maxWidth="1200px">
